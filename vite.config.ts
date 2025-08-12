7b7b08adb05c01bd578e90b1eee380207b291313
Prajwal-R-K <prajwalrk2004@gmail.com>
2025-08-12 23:54:58 +0530

    jhd

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,14 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-<<<<<<< HEAD
-  base: "/parllel_computing/",  // 👈 Add this
-=======
-  base: "/parllel_computing/", // ← Replace with your repo name
->>>>>>> a559842d2fbc7b14e00905e21345a66ed21ae556
+  base: "/parllel_computing/", // 👈 Repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-<<<<<<< HEAD
-    mode === 'development' && componentTagger(),
-=======
     mode === "development" && componentTagger(),
->>>>>>> a559842d2fbc7b14e00905e21345a66ed21ae556
   ].filter(Boolean),
   resolve: {
     alias: {
