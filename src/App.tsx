92833ccdfb14bc21c6e697a5c83add2934a33fe5
Prajwal-R-K <prajwalrk2004@gmail.com>
2025-09-03 00:11:07 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/openmp" element={<OpenMPPage />} />
